5bc0a189e21266994ebb7fae4634d47d24c9db00
Craig Dallimore <decoy9697@gmail.com>
2011-10-10 13:52:09 -0400

    First batch of files

---
+++ js/app.js
@@ -0,0 +1,30 @@
+﻿var app = {
+
+/*---------------------------------------------
+Configuration / Defaults
+---------------------------------------------*/
+  config: {
+
+
+  },
+
+  // everything else goes here
+
+
+  
+/*---------------------------------------------
+Initialisation
+---------------------------------------------*/
+  init: function () {
+
+    $('#btn-click').click(function (e) {
+      e.preventDefault();
+
+      console.log('JQUERY WORKS');
+
+    });
+
+
+  }
+
+};
---
+++ js/init.js
@@ -0,0 +1,42 @@
+/** =============================================================================
+	Doc style 1
+============================================================================== */
+
+/*------------------------------------------------------
+	Doc style 2
+	
+	// Self encapsulating function
+	(function($){
+
+		// jQuery Plugin
+		$.fn.plug_in = function ( options ) {
+		
+			// The defaults are used if the user has not 
+			// done any configuration.
+			var defaults = {
+			
+			},
+			
+			// jQuery Extend - add properties from the defaults
+			// to the options. Options override defaults.
+			options = $.extend( defaults , options );
+		
+		
+		};
+
+	})(jQuery);	
+	
+------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
+// jQuery Ignition
+$(document).ready(function() {
+
+});// jQuery
